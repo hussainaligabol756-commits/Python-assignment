9c367588961edcd53fc6d064f0a130f4ea6b21b5
Hussain Ali Gabol <hussainaligabol756@gmail.com>
2026-09-08 22:56:55 +0500

    Create Q5.py

---
+++ Q5.py
@@ -0,0 +1,10 @@
+Question5: 
+age =20 
+item=5  
+print("age:",age)      #age:20
+print("items:",items)  #items:5
+
+age=float(age)
+age=float(items)
+print("age:",age)      #age:20.0
+print("items:",items)  #items:5.0
