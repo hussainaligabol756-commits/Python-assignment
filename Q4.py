e1c389279714ea582e0105c56ff71e648442093e
Hussain Ali Gabol <hussainaligabol756@gmail.com>
2026-09-08 22:49:44 +0500

    Create Q4.py

---
+++ Q4.py
@@ -0,0 +1,7 @@
+Question4:
+num1=int(input("Enter the fisrt number: "));
+num2=int(input("Enter the second number: "));
+result=1
+for i in range (num2):
+  result *= first
+print(f"{first} raised to the power {second} is: {result}")
