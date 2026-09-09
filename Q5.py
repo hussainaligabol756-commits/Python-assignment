ec097ae907e135fd29a7a921bcb21c2d265bad3e
Hussain Ali Gabol <hussainaligabol756@gmail.com>
2026-09-09 21:42:33 +0500

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,10 +1,9 @@
-Question5: 
 age =20 
 item=5  
 print("age:",age)      #age:20
 print("items:",items)  #items:5
 
-age=float(age)
-age=float(items)
+age=float(age) # int into float
+age=float(items) #int into float
 print("age:",age)      #age:20.0
 print("items:",items)  #items:5.0
